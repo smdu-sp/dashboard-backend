049872dc1efcd8e56f593d3bc8b7b557f2b4d05c
brunoblv <blv.bruno@gmail.com>
2024-05-15 15:30:32 -0300

    inicial

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+async function main() {
+  const root = await prisma.usuario.upsert({
+    where: { login: 'd854440' },
+    create: {
+      login: 'd854440',
+      nome: 'Bruno Luiz Vieira',
+      email: 'blvieira@prefeitura.sp.gov.br',
+      status: 1,
+      permissao: 'DEV',
+    },
+    update: {
+      login: 'd854440',
+      nome: 'Bruno Luiz Vieira',
+      email: 'blvieira@prefeitura.sp.gov.br',
+      status: 1,
+      permissao: 'DEV',
+    },
+  });
+  console.log(root);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });

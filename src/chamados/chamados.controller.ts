f984bc526730b8a9132903d3f919c39f0fca43d2
Victor Abreu <aalevictor@gmail.com>
2024-05-16 17:00:08 -0300

    usuarios

--- src/chamados/chamados.controller.ts
+++ src/chamados/chamados.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ChamadosService } from './chamados.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { UsuarioAtual } from 'src/auth/decorators/usuario-atual.decorator';
+import { Usuario } from '@prisma/client';
 
 @Controller('chamados')
 export class ChamadosController {
@@ -8,8 +10,11 @@ export class ChamadosController {
   
   @IsPublic()
   @Get()
-  findAll( @Query('status') status: string) {
-    return this.chamadosService.findAll(+status);
+  findAll(
+    @UsuarioAtual() usuario: Usuario, 
+    @Query('status') status: string
+  ) {
+    return this.chamadosService.findAll(usuario, +status);
   }
 
   @Get(':id')
--- src/chamados/chamados.service.ts
+++ src/chamados/chamados.service.ts
@@ -1,14 +1,28 @@
+import { Usuario } from '@prisma/client';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Prisma3Service } from 'src/prisma3/prisma3.service';
 
 @Injectable()
 export class ChamadosService {
-
   constructor(private prisma: PrismaService, private prisma3: Prisma3Service) { }
-  async findAll(status: number) {
+  async findAll(usuario: Usuario, status: number) {
     const data = await this.prisma3.glpi_ticketsatisfactions.findMany({
       where: {
+        ...(usuario.permissao === 'USR' ? { 
+          Tickets: {
+            Usuarios: {
+              every: {
+                type: 1,
+                user: {
+                  name: {
+                    contains: usuario.login
+                  }
+                }
+              }
+            }
+          }
+        } : {}),
         ...(status === 0 ? { date_answered: null } : status === 1 ? { date_answered: { not: null } } : {})
       },
       include: {
